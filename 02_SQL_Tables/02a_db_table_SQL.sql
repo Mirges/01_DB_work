2368b09eb9dd9708a168757387571b75df1cac35
Mirko <mirkobicking@gmail.com>
2025-02-28 12:35:25 +0100

    Änderungen gemacht ++

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -1,3 +1,19 @@
+\! cls
+
+SHOW DATABASES;
+DROP DATABASE IF EXISTS test;
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+
+);
+SHOW TABLES;
+DESCRIBE test;
+INSERT INTO test(name,age) VALUES("Max", 35);
+INSERT INTO test(age,name) VALUES(29, "Maxine");
+INSERT INTO test VALUES();
+SELECT * FROM test;
 /* ------  Strukturen ----- */
 
 /* Kommentar 1 */
