00985d2e19d1dc8774e0a9226232a5b4ce00e3f7
Mirko <mirkobicking@gmail.com>
2025-03-03 08:48:32 +0100

    Änderung ++

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -14,33 +14,4 @@ INSERT INTO test(name,age) VALUES("Max", 35);
 INSERT INTO test(age,name) VALUES(29, "Maxine");
 INSERT INTO test VALUES();
 SELECT * FROM test;
-/* ------  Strukturen ----- */
 
-/* Kommentar 1 */
-#  Kommentar 2
--- Kommentar 3
-
-/* Datenbanken auf Server anzeigen */
-
-
-/* DB boo löschen, falls vorhanden*/
-
-
-/* DB boo anlegen, falls noch nicht vorhanden*/
-
-
-/* DB auswählen */
-
-/* Tabelle anlegen */
-
-
-/* Alle Tabellen in der DB anzeigen */
-
-
-/* Struktur der Tabelle anzeigen */
-
-
-/* ----- Daten ------- */
-
-
-/* ---- Inhalte der Tabelle anzeigen ---- */
